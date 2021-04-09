acf9867a6cd39d64488e0f66e34fbda9488595de
github username <github email address>
2021-04-09 13:11:20 -0400

    staircase solved

--- staircase.js
+++ staircase.js
@@ -18,26 +18,3 @@ function staircase(n) {
 staircase(6)
 
 
-// function steps(n) {
-//     // iterate through rows from 0 to n
-//     for(let row = 0; row < n; row++){
-//         let stair = ''; //created an empty string
-        
-//         for(let column = 0; column < n; column++){
-//            //for each column, if the current column is less than or equal to the current row
-//             if(column <= row) {
-//                 stair += '#'; //then we want to add a pound symbol to the stari string
-//             } else {
-//                 stair += ' '; //otherwise add a space
-//             }
-//         }
-//        //after we process one entire row above we need to console log it
-//         console.log(stair)
-//     }
-
-//     //second solution with recursion only do this in interviews if asked to solve with recursion
-//     //define my basecase. Check more in udeamy video
-// //     if(n === row){
-// //         return;
-// //     }
-// }
